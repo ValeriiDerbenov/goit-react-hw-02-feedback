61100ff26fd95df25d3807c47639a6743f5c2010
Valerii <vd.alternative@gmail.com>
2023-12-29 21:39:12 +0200

    title1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Section } from "components/Section/Section";
+import { Component } from "react"
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render()
+  {return (
+    <Section title="Please leave feedback">
+
+    </Section>
+      
+  );}
+};
