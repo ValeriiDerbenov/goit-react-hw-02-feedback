f618462f3e3a11784ca39d7187b23dcddcd17230
Valerii <vd.alternative@gmail.com>
2024-01-06 11:58:00 +0200

    css

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -6,7 +6,7 @@ export const FeedbackOptions =({onClick, feedbackOptions}) => {
 	return (
 		<ButtonWrapper>
 			{Object.keys(feedbackOptions).map((key, index) => {
-				console.log('object :>> ', index);
+				// console.log('object :>> ', index);
 				return(
 					<button
 					className="optionButton"
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,18 +1,19 @@
+import { Box, Grid } from "./Statistics.styled"
 
 export const Statistics = ({title, good, neutral, bad, total, positivePercentage}) => {
 
 	return(
-		<>
-		<div>
+		<Box>
 			<h2>{title}</h2>
+			<Grid>
 			<p>Good: {good}</p>
 			<p>Neutral: {neutral}</p>
 			<p>Bad: {bad}</p>
-		</div>
+		</Grid>
 		<div>
 			<p>Total: {total}</p>
 			<p>Positive: {positivePercentage}%</p>
 		</div>
-		</>
+		</Box>
 	)
 }
